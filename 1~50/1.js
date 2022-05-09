186373cce8f769d0567bea360581c03fa9884776
SangYoonLee <sylagape1231@gmail.com>
2022-05-09 22:15:24 +0900

    Update All
    2022-05-09 JS 100Q
    (1~3)

---
+++ 1~50/1.js
@@ -0,0 +1,11 @@
+// 문제 1 : 배열의 삭제
+// https://www.notion.so/1-94677631c7b642a7bf3a60d93137f7b5
+
+let nums = [100, 200, 300, 400, 500];
+
+nums.pop();
+nums.pop();
+
+console.log(nums);
+
+// 배열의 내장 함수 pop을 아용하여 배열의 마지막 요소를 제거할 수 있다.
---
+++ 1~50/3.js
@@ -0,0 +1,8 @@
+// 문제 3 : 변수의 타입
+// https://www.notion.so/3-8c063316324b42239786fd2c3e7bc105
+
+let arr = [100, 200, 300];
+
+console.log(typeof arr); // object
+
+// JS에서 배열의 타입은 객체이다.
